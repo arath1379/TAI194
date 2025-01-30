5da060f23e907058bf0d0f13405527810088724d
arath1379 <122041683@upq.edu.mx>
2025-01-29 18:10:19 -0600

    Practica 2

--- FAST/main.py
+++ FAST/main.py
@@ -6,3 +6,14 @@ app= FastAPI()
 @app.get('/')
 def home():
     return {'hello': 'world fastApi'}
+
+#ruta o ENDPOINT Promedio
+@app.get('/promedio')
+def promedio():
+    return 10
+
+#ruta o ENDPOINT con parametro obligatorio
+@app.get('/usuario/{id}')
+def consultausuario(id:int):
+    #caso ficticio de busqueda de la BD
+    return {"Se encontro el usuario":id}
