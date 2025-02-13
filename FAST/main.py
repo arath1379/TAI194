d8ab25d6d294b936b4f38ea031684590418666f7
arath1379 <122041683@upq.edu.mx>
2025-02-12 18:05:14 -0600

    Practica 3 Parte final

--- FAST/main.py
+++ FAST/main.py
@@ -41,3 +41,12 @@ def actualizar(id:int,usuarioActualizado:dict):
             usuarios[index].update(usuarioActualizado)
             return usuarios[index]
     raise HTTPException(status_code=400,detail="El usuario no existe")
+
+#ENDPOINT para eliminar
+@app.delete('/usuarios/{id}',tags=['Operaciones CRUD'])
+def eliminar(id:int):
+    for index, usr in enumerate(usuarios):
+        if usr['id'] == id:
+            usuarios.pop(index)
+            return {'Mensaje':'Usuario eliminado con exito'}
+    raise HTTPException(status_code=400,detail="El usuario no existe")
