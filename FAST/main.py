46496bb4ac23f1b53aa2fa8955c8710b0fda1c06
arath1379 <122041683@upq.edu.mx>
2025-02-28 15:28:58 -0600

    Avance II Practica 6

--- FAST/main.py
+++ FAST/main.py
@@ -1,54 +1,65 @@
 from fastapi import FastAPI, HTTPException
 from typing import Optional, List
 from models import modelUsuario
+from models import modelAuth
+from genToken import createToken
 
-app= FastAPI(
+app = FastAPI(
     title="Mi primer API",
     description="Arath Josue Perez Campos",
     version="1.0.0",
 )
 
-
-usuarios=[
-    {"id":1,"nombre":"Arath","edad":20,"correo":"arath@example.com"},
-    {"id":2,"nombre":"Josue","edad":22,"correo":"josue@example.com"},
-    {"id":3,"nombre":"Jacinto","edad":25,"correo":"jaci@example.com"},
-    {"id":4,"nombre":"Joel","edad":21,"correo":"joel@example.com"}
+usuarios = [
+    {"id": 1, "nombre": "Arath", "edad": 20, "correo": "arath@example.com"},
+    {"id": 2, "nombre": "Josue", "edad": 22, "correo": "josue@example.com"},
+    {"id": 3, "nombre": "Jacinto", "edad": 25, "correo": "jaci@example.com"},
+    {"id": 4, "nombre": "Joel", "edad": 21, "correo": "joel@example.com"},
 ]
 
-#ruta o ENDPOINT
-@app.get('/',tags=['Inicio'])
+# Ruta de inicio
+@app.get("/", tags=["Inicio"])
 def home():
-    return {'hello': 'world fastApi'}
+    return {"hello": "world fastApi"}
 
-#ENDPOINTConsulta Todos
-@app.get('/todosUsuarios',response_model=List[modelUsuario],tags=['Operaciones CRUD'])
+# Endpoint para generar token
+@app.post("/auth", tags=["Autenticación"])
+def auth(credenciales: modelAuth):
+    if credenciales.mail == "arath@example.com" and credenciales.passw == "123456789":
+        token: str = createToken(credenciales.model_dump())
+        print(token)
+        return {"Aviso": "Token generado"}
+    else:
+        return {"Aviso": "Usuario no cuenta con permiso"}
+
+# ENDPOINT - Obtener todos los usuarios
+@app.get("/todosUsuarios", response_model=List[modelUsuario], tags=["Operaciones CRUD"])
 def leer():
     return usuarios
 
-#ENDPOINT POST
-@app.post('/usuarios/',response_model=modelUsuario,tags=['Operaciones CRUD'])
-def guardar(usuario:modelUsuario):
+# ENDPOINT - Agregar usuario
+@app.post("/usuarios/", response_model=modelUsuario, tags=["Operaciones CRUD"])
+def guardar(usuario: modelUsuario):
     for usr in usuarios:
-        if usr['id'] == usuario.id:
-            raise HTTPException(status_code=400,detail="El usuario ya existe")
-    usuarios.append(usuario)
+        if usr["id"] == usuario.id:
+            raise HTTPException(status_code=400, detail="El usuario ya existe")
+    usuarios.append(usuario.model_dump())
     return usuario
 
-#ENDPOINT para actualizar
-@app.put('/usuarios/{id}',response_model=modelUsuario,tags=['Operaciones CRUD'])
-def actualizar(id:int,usuarioActualizado:modelUsuario):
+# ENDPOINT - Actualizar usuario
+@app.put("/usuarios/{id}", response_model=modelUsuario, tags=["Operaciones CRUD"])
+def actualizar(id: int, usuarioActualizado: modelUsuario):
     for index, usr in enumerate(usuarios):
-        if usr['id'] == id:
-            usuarios[index]== usuarioActualizado.model_dump()
+        if usr["id"] == id:
+            usuarios[index] = usuarioActualizado.model_dump()
             return usuarios[index]
-    raise HTTPException(status_code=400,detail="El usuario no existe")
+    raise HTTPException(status_code=400, detail="El usuario no existe")
 
-#ENDPOINT para eliminar
-@app.delete('/usuarios/{id}',tags=['Operaciones CRUD'])
-def eliminar(id:int):
+# ENDPOINT - Eliminar usuario
+@app.delete("/usuarios/{id}", tags=["Operaciones CRUD"])
+def eliminar(id: int):
     for index, usr in enumerate(usuarios):
-        if usr['id'] == id:
+        if usr["id"] == id:
             usuarios.pop(index)
-            return {'Mensaje':'Usuario eliminado con exito'}
-    raise HTTPException(status_code=400,detail="El usuario no existe")
+            return {"Mensaje": "Usuario eliminado con éxito"}
+    raise HTTPException(status_code=400, detail="El usuario no existe")
